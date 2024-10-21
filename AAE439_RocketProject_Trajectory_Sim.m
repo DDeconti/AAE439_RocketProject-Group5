fa60a38dd43dae7aea54d84ee5130219378622a5
DDeconti <59516793+DDeconti@users.noreply.github.com>
2024-10-21 18:14:02 -0400

    Started file
    Made new file

---
+++ AAE439_RocketProject_Trajectory_Sim.m
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Course: AAE43900 - 001
+
+% Date: October 27, 2024
+
+% Co-Authors:
+% Daniel DeConti
+% Ryan Morey
+
+% Rocket Project
+
+% Description: model the trajectory of a small rocket with a solid
+% propellant motor.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+format shortg
+format compact
+close all
+clear
+clc
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Inputs/Constants:
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
